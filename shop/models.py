2146da384274b0f7af298a1c7ec510175dc1ae4d
Maksym Rakomin <maksym.rakomin@gmail.com>
2024-02-26 23:52:07 +0200

    create mirgaitons

--- shop/models.py
+++ shop/models.py
@@ -8,7 +8,7 @@ class Category(models.Model):
 
 
 class Course(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=300)
     price = models.FloatField()
     reviews_qty = models.IntegerField()
     students_qty = models.IntegerField()
