c450e0a992ce9762845a05485b711145e1582eee
Maksym Rakomin <maksym.rakomin@gmail.com>
2024-02-26 23:32:51 +0200

    first commit

---
+++ shop/models.py
@@ -0,0 +1,16 @@
+from django.utils import timezone
+from django.db import models
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(default=timezone.now)
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.FloatField()
+    reviews_qty = models.IntegerField()
+    students_qty = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
