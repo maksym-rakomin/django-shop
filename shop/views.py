60644599a43812fc8d91d751d38245393151f906
Maksym Rakomin <maksym.rakomin@gmail.com>
2024-02-27 12:46:18 +0200

    add models to admin board + add output titles to site

--- shop/views.py
+++ shop/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Course, Category
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello form the shop app!")
+    courses = Course.objects.all()
+    res = f"<ul>{
+        ''.join(['<li>' + str(course) + '</li>' for course in courses])
+    }</ul>"
+    return HttpResponse(res)
