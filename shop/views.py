79050b4f74002fe97f955fc243a8e94955e746b2
Maksym Rakomin <maksym.rakomin@gmail.com>
2024-02-27 23:14:57 +0200

    add named routing to app

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from .models import Course, Category
 
@@ -10,6 +10,7 @@ def index(request):
 
 
 def single_course(request, course_id):
+    # OPTION 1
     # try:
     #     course = Course.objects.get(pk=course_id)
     #     return render(request, 'single_course.html', {'course': course})
@@ -17,5 +18,6 @@ def single_course(request, course_id):
     # except Course.DoesNotExist:
     #     raise Http404()
 
-    course = get_list_or_404(Course, pk=course_id)
+    # OPTION 2
+    course = get_object_or_404(Course, pk=course_id)
     return render(request, 'single_course.html', {'course': course})
