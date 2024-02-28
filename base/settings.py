9f5c637506d59f2b9bd2e82d68397a92b2a5dbec
Maksym Rakomin <maksym.rakomin@gmail.com>
2024-02-28 10:16:51 +0200

    refactoring

--- base/settings.py
+++ base/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'base.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates', BASE_DIR / 'templates' / 'shop'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
